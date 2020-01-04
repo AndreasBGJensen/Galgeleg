8c2441aaa9bfc47109f7a44f185a71fd3177552d
AndreasBGJensen <bgandreas@hotmail.com>
2020-01-04 22:36:58 +0100

    Add TODO: Change scoring

--- app/src/main/java/com/example/galgeleg/Model/Spillogik/HighScore/CalculatScore.java
+++ app/src/main/java/com/example/galgeleg/Model/Spillogik/HighScore/CalculatScore.java
@@ -7,6 +7,8 @@ public class CalculatScore {
 
     private int[] scoreList = {10,9,8,7,6,5,4,3,2,1,0};
 
+    //TODO: Change this highScore so that it only uses the guesses that are longer than the length of the words.
+
     public CalculatScore(String currentScore, String newScorePoints){
         this.currentScore=Integer.parseInt(currentScore);
         if(newScorePoints==null){
